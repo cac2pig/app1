7d63b2774908ac05774719b7a4038a2173bec4db
Sean <sean@swat.biz>
2017-08-18 16:49:18 +0800

    投票功能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :topics
-  root "topics#index"
+  resources :topics do
+    member do
+      post 'upvote'
+    end
+  #root "topics#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
