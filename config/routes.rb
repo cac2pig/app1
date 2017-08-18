a097bd8f7770a0f520c4c2b13c745ca835584390
Sean <sean@swat.biz>
2017-08-18 17:20:17 +0800

    修改route“

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
       post 'upvote'
     end
   root "topics#index"
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
